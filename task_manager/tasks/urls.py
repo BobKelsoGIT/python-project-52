87847182e0e8ed76d2212a7298b116dfce014f71
Ivan Scherbakov <lolwut155@gmail.com>
2024-08-30 21:27:28 +0300

    tasks/urls.py routes added

---
+++ task_manager/tasks/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import ListTaskView, CreateTaskView, UpdateTaskView, DeleteTaskView
+
+urlpatterns = [
+    path('', ListTaskView.as_view(), name='tasks_list'),
+    path('create/', CreateTaskView.as_view(), name='task_create'),
+    path('<int:pk>/update/', UpdateTaskView.as_view(), name='task_update'),
+    path('<int:pk>/delete/', DeleteTaskView.as_view(), name='task_delete'),
+]
