f333a4933503c9c96a508e8d66555a781f5282b0
Ivan Scherbakov <lolwut155@gmail.com>
2024-08-30 13:19:41 +0300

    views.py views added

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,23 @@
 from django.views.generic.base import TemplateView
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 
 
 class IndexView(TemplateView):
-    template_name = "base.html"
+    template_name = 'index.html'
+
+
+class UserLoginView(SuccessMessageMixin, LoginView):
+    template_name = 'form.html'
+    next_page = 'home'
+    success_message = _('Logged in successfully!')
+
+
+class UserLogoutView(SuccessMessageMixin, LogoutView):
+    success_message = _('Logged out sucessfully.')
+    next_page = reverse_lazy('home')
+
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
