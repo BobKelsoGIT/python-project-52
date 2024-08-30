ff7e1a9631830ad623e7338e4382c82e23a54734
Ivan Scherbakov <lolwut155@gmail.com>
2024-08-30 13:19:07 +0300

    users/views.py views added

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import User
+from django.utils.translation import gettext_lazy as _
+from .signup_form import CreateUserForm
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 
-# Create your views here.
+
+class UsersListView(ListView):
+    model = User
+    template_name = 'users/users_list.html'
+    context_object_name = 'users'
+    extra_context = {
+        'title': _('Users')
+    }
+
+
+class UserCreateView(SuccessMessageMixin, CreateView):
+    model = User
+    form_class = CreateUserForm
+    template_name = 'form.html'
+    success_url = reverse_lazy('login')
+    success_message = _('User successfully created')
+
+
+class UserUpdateView(UpdateView):
+    pass
+
+
+class UserDeleteView(DeleteView):
+    pass
