1a8631898198bf0a5006aef5f305823eb15d925c
Ivan Scherbakov <lolwut155@gmail.com>
2024-09-02 18:07:03 +0300

    users/models.py meta verbose added

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
