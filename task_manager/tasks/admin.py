b144d92abfebb1dbd14d3a3392128ddd4712dfe0
Ivan Scherbakov <lolwut155@gmail.com>
2024-09-02 19:19:32 +0300

    admin models added

--- task_manager/tasks/admin.py
+++ task_manager/tasks/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Task, TaskLabelRelation
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'author', 'date_created', 'status', 'executor',
+        'display_labels',
+    )
+
+    def display_labels(self, obj):
+        labels = (TaskLabelRelation.objects.filter
+                  (task=obj).select_related('label'))
+        return ', '.join(
+            [label_relation.label.name for label_relation in labels]
+        )
+
+    display_labels.short_description = 'Labels'
+
+
+@admin.register(TaskLabelRelation)
+class TaskRelationsAdmin(admin.ModelAdmin):
+    list_display = ('task', 'label')
