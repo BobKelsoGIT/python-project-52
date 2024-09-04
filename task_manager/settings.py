82f94a4cc53a820ed1305037c11b0658cd251c68
Ivan Scherbakov <lolwut155@gmail.com>
2024-09-04 19:48:03 +0300

    locale test

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -148,9 +149,7 @@ ROLLBAR = {
     'root': str(BASE_DIR),
 }
 
-LOCALE_PATHS = [
-    os.path.join(BASE_DIR, './locale'),
-]
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
