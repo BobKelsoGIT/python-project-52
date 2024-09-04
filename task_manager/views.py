d6fa8f4a235eee7f65c864ee7e4ceb125272785e
Ivan Scherbakov <lolwut155@gmail.com>
2024-09-04 21:03:35 +0300

    UserLogoutView flash message updated

--- task_manager/views.py
+++ task_manager/views.py
@@ -22,7 +22,6 @@ class UserLoginView(SuccessMessageMixin, LoginView):
 
 class UserLogoutView(SuccessMessageMixin, LogoutView):
     next_page = reverse_lazy('home')
-    success_message = _('Logged out successfully!')
 
     def dispatch(self, request, *args, **kwargs):
         messages.info(request, _('Logged out successfully!'))
