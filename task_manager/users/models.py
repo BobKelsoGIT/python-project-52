1a41ec0a8b66e72032d6ef66b7c08a6f8b70bdef
Ivan Scherbakov <lolwut155@gmail.com>
2024-09-05 01:11:47 +0300

    users/models.py __str__ added

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -10,3 +10,6 @@ class User(AbstractUser):
     class Meta:
         verbose_name = _('User')
         verbose_name_plural = _('Users')
+
+    def __str__(self):
+        return self.get_full_name()
