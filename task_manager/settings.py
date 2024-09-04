5d2559cadd608c9f079da769a18133f185cadc72
Ivan Scherbakov <lolwut155@gmail.com>
2024-09-04 18:06:17 +0300

    settings.py language

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -145,7 +146,7 @@ ROLLBAR = {
 }
 
 LOCALE_PATHS = [
-    os.path.join(BASE_DIR, './locale'),
+    os.path.join(BASE_DIR, 'locale'),
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
