8cad6defd84c83d2bd12de8572d78a220b07ac7e
Ivan Scherbakov <lolwut155@gmail.com>
2024-09-06 00:54:52 +0300

    users/models.py Unnecessary overrides removed

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,12 +1,8 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-
     class Meta:
         verbose_name = _('User')
         verbose_name_plural = _('Users')
