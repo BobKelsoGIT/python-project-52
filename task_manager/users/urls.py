e0696c4188351cfbf109b893b2ecf261cc99a354
Ivan Scherbakov <lolwut155@gmail.com>
2024-09-10 13:45:22 +0300

    url 'signup' renamed to 'user_create'

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -4,7 +4,7 @@ from .views import UserCreateView, UserDeleteView, UsersListView, UserUpdateView
 
 urlpatterns = [
     path('', UsersListView.as_view(), name='users'),
-    path('create/', UserCreateView.as_view(), name='signup'),
+    path('create/', UserCreateView.as_view(), name='user_create'),
     path('<int:pk>/update/', UserUpdateView.as_view(), name='user_update'),
     path('<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
 ]
